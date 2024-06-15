63831ce8076d5cc2cb6953fd06c932e2b987a440
Malay Pandit <malay.pandit@dinereel.com>
2024-06-15 23:23:21 +0530

    added ios notification setup

--- lib/remote/remote.dart
+++ lib/remote/remote.dart
@@ -7,6 +7,4 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:injectable/injectable.dart';
 
-import '../presentation/screens/home_screen_v2/cubit/download_cubit.dart';
-
 part 'client/download_file_client.dart';
